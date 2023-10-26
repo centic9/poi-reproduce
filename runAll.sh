f06612cd7d074d11f6593998d22b52fce6cc8d7c
Dominik Stadler <dominik.stadler@gmx.at>
2023-10-26 15:03:01 +0200

    Add Gradle task and adjust runAll.sh

--- runAll.sh
+++ runAll.sh
@@ -3,5 +3,16 @@
 for ver in 4.1.2 5.0.0 5.1.0 5.2.0 5.2.1 5.2.2 5.2.3 5.2.4;do
   echo
   echo Testing version ${ver}
-  ./gradlew run -PpoiVersion=${ver}
+
+  ./gradlew runClose -PpoiVersion=${ver} -PcloseTest=stream
+  ./gradlew runClose -PpoiVersion=${ver} -PcloseTest=file
+  ./gradlew runClose -PpoiVersion=${ver} -PcloseTest=path
+
+  ./gradlew runClose -PpoiVersion=${ver} -PcloseTest=opcstream
+  ./gradlew runClose -PpoiVersion=${ver} -PcloseTest=opcfile
+  ./gradlew runClose -PpoiVersion=${ver} -PcloseTest=opcpath
+
+  ./gradlew runClose -PpoiVersion=${ver} -PcloseTest=specialstream
+  ./gradlew runClose -PpoiVersion=${ver} -PcloseTest=specialfile
+  ./gradlew runClose -PpoiVersion=${ver} -PcloseTest=specialpath
 done
