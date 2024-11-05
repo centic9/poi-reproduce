1090c09b28d1f650051295d0593163ef0e877d6b
Dominik Stadler <dominik.stadler@gmx.at>
2024-11-05 21:22:29 +0100

    Add option to set different Zip64Mode

--- src/main/java/org/dstadler/poi/TestSXSSFWorkbook.java
+++ src/main/java/org/dstadler/poi/TestSXSSFWorkbook.java
@@ -3,6 +3,7 @@ package org.dstadler.poi;
 import java.io.FileOutputStream;
 import java.io.IOException;
 
+import org.apache.commons.compress.archivers.zip.Zip64Mode;
 import org.apache.poi.Version;
 import org.apache.poi.xssf.streaming.SXSSFWorkbook;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
@@ -11,7 +12,7 @@ public class TestSXSSFWorkbook {
 
 	public static void main(String[] args) throws IOException {
 		if (args.length == 0) {
-			System.err.println("Usage: TestXSSFWorkbook <file> [<bool-compressTemp> <bool-sharedStringTable> <windowSize>]");
+			System.err.println("Usage: TestXSSFWorkbook <file> [<bool-compressTemp> <bool-sharedStringTable> <windowSize> <Zip64Mode>]");
 			System.exit(1);
 		}
 
@@ -20,11 +21,20 @@ public class TestSXSSFWorkbook {
 		boolean compressTemp = args.length == 1 || Boolean.parseBoolean(args[1]);
 		boolean sharedStringTable = args.length <= 2 || Boolean.parseBoolean(args[2]);
 		int windowSize = args.length > 3 ? Integer.parseInt(args[3]) : 100;
+		Zip64Mode zipMode = args.length > 4 ? Zip64Mode.valueOf(args[4]) : null;
 
 		System.out.println();
-		System.out.println("Writing to " + file + " with compressTemp: " + compressTemp + " and sharedStringTable: " + sharedStringTable + " for version " + Version.getVersion() + " and Java " + System.getProperty("java.version"));
+		System.out.println("Writing to " + file + " with compressTemp: " + compressTemp +
+				", sharedStringTable: " + sharedStringTable +
+				", windowSize: " + windowSize +
+				", zipMode: " + zipMode +
+				" for version " + Version.getVersion() +
+				" and Java " + System.getProperty("java.version"));
 
 		SXSSFWorkbook wb  = new SXSSFWorkbook(new XSSFWorkbook(), windowSize, compressTemp, sharedStringTable);
+		if (zipMode != null) {
+			wb.setZip64Mode(zipMode);
+		}
 
 		wb.createSheet("test")/*.createRow(0).createCell(0).setCellValue("string")*/;
 
